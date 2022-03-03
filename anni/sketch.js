dcd25e1e93592e31c73cd501be033a1580ce64dc
Dan Calacci <dcalacci@media.mit.edu>
2022-03-02 19:45:42 -0500

    Generative sketches, at least

--- anni/sketch.js
+++ anni/sketch.js
@@ -3,35 +3,108 @@ let h = 600;
 
 let distances = [];
 let maxDistance;
-let spacer;
+
+let dotDensity = 1;
+let spacer = w / 10;
+
+let grid = [];
+let latestMove = [spacer, spacer];
+let moveList = [latestMove]
+
 
 function setup() {
   pixelDensity(1);
   createCanvas(8.5 * 150, 11 * 150); // create an 8.5x11 inch canvas at 150dpi
-
-  maxDistance = dist(width / 2, height / 2, width, height);
-  for (let x = 0; x < width; x++) {
-    distances[x] = []; // create nested array
-    for (let y = 0; y < height; y++) {
-      let distance = dist(width / 2, height / 2, x, y);
-      distances[x][y] = (distance / maxDistance) * 255;
+  for (let x = 0; x < width; x += spacer) {
+    grid[x] = [];
+    for (let y = 0; y < height; y += spacer) {
+      grid[x][y] = 0;
     }
   }
-  spacer = 10;
-
-  noLoop();
+  grid[spacer][spacer] = 1;
+  // noLoop();
+  rectMode(CENTER);
 }
 
- 
+function canMove(x, y) {
+  // if x and y are within the width and height and x and y are at least 1 away
+  // from another dot, return true
+  surroundingsAreEmpty = (x, y) => {
+    // returns true if only one surrounding dot is filled
+    let surroundings = [
+      grid[x + spacer][y],
+      grid[x - spacer][y],
+      grid[x][y + spacer],
+      grid[x][y - spacer],
+      grid[x+spacer][y+spacer],
+      grid[x-spacer][y-spacer]
+    ];
+    return surroundings.filter((s) => s === 1).length <= 2;
+  };
+
+  return (
+    x >= spacer && x < width-spacer && y >= spacer && y < height-spacer && surroundingsAreEmpty(x, y)
+  );
+}
+
+function makeMove(x,y, direction) {
+    let newX = x;
+    let newY = y;
+    switch (direction) {
+      case 0:
+        newX += spacer;
+        break;
+      case 1:
+        newX-= spacer;
+        break;
+      case 2:
+        newY+= spacer;
+        break;
+      case 3:
+        newY-= spacer;
+        break;
+    }
+    return [newX, newY]
+}
+
+function generateMove(x, y) {
+  let directions = [0, 1, 2, 3]; // right, left, up, down
+  let legalMoves = directions.filter((direction) => {
+    let d = makeMove(x, y, direction)
+    return canMove(d[0], d[1])
+  });
+  if (legalMoves.length == 0) {
+    return false
+  }
+  return random(legalMoves)
+}
+
 
 function draw() {
   // Loop through creating line segments
+  // TODO: segments should be at least two long
   beginShape();
-  noFill();
+  fill(230, 0, 76);
+  noStroke();
   for (let x = 0; x < width; x += spacer) {
     for (let y = 0; y < height; y += spacer) {
-      stroke(distances[x][y]);
-      point(x + spacer / 2, y + spacer / 2);
+      if (grid[x][y] === 1) {
+        rect(x, y, spacer - 1, spacer - 1);
+      }
     }
+  }
+  let latestMove = moveList[moveList.length - 1];
+  let move = generateMove(latestMove[0], latestMove[1]);
+  if (move === false) {
+    console.log("can't move, retracing...")
+    // let poppedMove = moveList.pop();
+    console.log(moveList.length)
+    moveList = moveList.slice(0, moveList.length - 2);
+    console.log(moveList.length)
+  } else {
+    let [newX, newY] = makeMove(latestMove[0], latestMove[1], move);
+    grid[newX][newY] = 1;
+    moveList.push([newX, newY])
+
   }
 }
