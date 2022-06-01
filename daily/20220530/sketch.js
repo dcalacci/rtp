f5e667a98546a8b09b05a309713039c0d873dd47
Dan Calacci <dcalacci@media.mit.edu>
2022-06-01 18:29:51 -0400

    save5

--- daily/20220530/sketch.js
+++ daily/20220530/sketch.js
@@ -1,4 +1,4 @@
-let divs = 10;
+let divs = 4;
 let size = 250;
 let minHeight = 20;
 let maxHeight = 120;
@@ -19,20 +19,10 @@ function setup() {
   } else {
     noStroke();
   }
-  ortho(-width / 2, width / 2, -height / 2, height / 2, -2 * size, 5 * size);
+  ortho(-width / 2, width / 2, -height / 2, height / 2, -2 * size, 6 * size);
   camera(width, -width / 2, width, 0, 0, 0, 0, 1, 0);
   //noLoop();
 }
-
-function getTranslation(x, y, z, t, h) {
-  let vertOffset = side - spacing;
-  translate(
-    x + side + h / 4,
-    z + side + h / 2,
-    y + side + h / 2)
-  // (-spacing + y + side / z));
-}
-
 
 
 function draw() {
@@ -45,7 +35,7 @@ function draw() {
   // directionalLight(255, 255, 0, 0, 0, -1);
   // directionalLight(255, 255, 0, 0, 0, -1);
   orbitControl();
-  for (let z = 0; z < divs; z++) {
+  for (let z = 0; z < divs * 5; z++) {
     for (let y = 0; y < divs; y++) {
       for (let x = 0; x < divs; x++) {
         let yPos = map(y, 0, divs, -size / 2, size / 2)
@@ -64,21 +54,44 @@ function draw() {
         //   (x + side / z),
         //   ((z * (side + vertOffset)) - 100) + h / 2,
         //   (-spacing + y + side / z)); //* (sin(frameCount / 50)))
+        let hh = (cos(
+          (frequency * TAU * z) /
+          width + frameCount / 30) / 2 + 0.1);
+
         box(
-          side - spacing / 2, //* map(getH(x + side / 2, y + side / 2, z), 0, 1, 1, 1.2),
-          side - spacing / 2, //* map(getH(xPos + side / 2, yPos + side / 2, z), 0, 1, 1, 1.2),
-          side - spacing / 2);
+          (side - spacing / 2) * hh * 2, //* map(getH(x + side / 2, y + side / 2, z), 0, 1, 1, 1.2),
+          (side - spacing / 2) * map(hh, 0, 1, .5, 1.2),
+          (side - spacing / 2));
         pop();
       }
     }
   }
 }
+function getTranslation(x, y, z, t, h) {
+  let vertOffset = side - spacing;
+  translate(
+    x + side,
+    y + side + h,
+    z - 400,
+  )
+  // (-spacing + y + side / z));
+}
+
+
+
 
 function getH(x, y, z) {
   let distFromCenter = dist(0, 0, 0, x, y, z);
+  let d;
+  d = z;
+  // } else if (frameCount < 20 * 30) {
+  //   d = z;
+  // } else if (frameCount < 30 * 30) {
+  //   d = distFromCenter
+  // }
   return (
     cos(
-      (frequency * PI / 2 * distFromCenter) /
+      (frequency * PI / 2 * d) /
       width + frameCount / 30) / 2 +
     0.1
   );
