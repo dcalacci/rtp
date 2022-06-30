a0162e3693cbef08f48303322cc51d879d3e4b31
Dan Calacci <dcalacci@media.mit.edu>
2022-06-30 22:38:08 +0100

    Nice brush

--- daily/20220630/sketch.js
+++ daily/20220630/sketch.js
@@ -27,7 +27,7 @@ function setup() {
 
   rectMode(CENTER);
 
-  c = createGraphics(width * 0.9, 500)
+  c = createGraphics(width * 0.9, height * 0.9)
 }
 
 function draw() {
@@ -37,18 +37,51 @@ function draw() {
 
   push()
   c.background(240)
-  c.translate(c.width * 0.1, c.width * 0.1)
+  c.translate(c.width * 0.1, c.height * 0.075)
+  let w = c.width * 0.8
+  let h = c.height / 5
+
+  drawPaintedRect({
+    width: w,
+    height: h,
+    canvas: c,
+    density: 0.8,
+    jitter: 2.2,
+    color: color(269, 11, 87)
+  })
+
+  c.translate(0, h + c.height * 0.1)
   drawPaintedRect({
     width: c.width * 0.8,
-    height: c.height / 5,
+    height: c.height / 10,
     canvas: c,
-    density: 0.4,
-    jitter: 1,
-    color: color(269, 11, 87)
-    // color: "#D1C5DE"
+    density: 0.7,
+    jitter: 1.8,
+    color: color(171, 16, 78)
+  })
+
+  c.translate(0, c.height / 10 + c.height * 0.1)
+  drawPaintedRect({
+    width: c.width * 0.8,
+    height: c.height / 8,
+    canvas: c,
+    density: 0.6,
+    jitter: 1.5,
+    color: color(0, 13, 90)
+  })
+
+  c.translate(0, c.height / 8 + c.height * 0.1)
+  drawPaintedRect({
+    width: c.width * 0.8,
+    height: c.height / 8,
+    canvas: c,
+    density: 0.5,
+    jitter: 1.8,
+    // color: color(222, 90, 50)
+    color: color(222, 73, 50)
   })
   pop()
-  image(c, width * 0.05, width * 0.05)
+  image(c, width * 0.05, height * 0.05)
 }
 
 
@@ -68,17 +101,21 @@ drawPaintedRect = ({ canvas, width, height, density, jitter, color }) => {
   let nLines = Math.ceil(h / lineWeight) + 1
 
   _.range(nLayers).forEach((i) => {
-    push()
+    canvas.push()
     canvas.translate(0, jt() / 2)
-    canvas.rotate(jt() * 0.001) // rotate ever so slightly
     _.range(nLines).forEach((n) => {
       if (d3.randomBernoulli(1 / 3)() > 0) {
+        //TODO: stray and errant lines here really bother me.
+        // an alternative approach is to generate the start and end positions,
+        // filter them for outliers, and then draw.
+        canvas.push()
         canvas.line(
-          0 - jt(), n * lineWeight,
-          w + jt(), n * lineWeight)
+          0 - jt(), n * lineWeight + jt(),
+          w + jt(), n * lineWeight + jt())
+        canvas.pop()
       }
     })
-    pop()
+    canvas.pop()
   })
 
   pop()
